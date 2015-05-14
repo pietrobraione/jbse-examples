897994c8248efeb9afaed235d259ba4a5a13533d
Pietro Braione <pietro.braione@gmail.com>
2015-05-14 13:39:38 +0200

    Add assertions

--- src/smalldemos/reflect/ReflectDemo.java
+++ src/smalldemos/reflect/ReflectDemo.java
@@ -1,5 +1,7 @@
 package smalldemos.reflect;
 
+import static jbse.meta.Analysis.ass3rt;
+
 import java.util.LinkedList;
 
 public class ReflectDemo {
@@ -7,16 +9,29 @@ public class ReflectDemo {
 	public static boolean equal;
 	public static boolean sameIntern;
 	public static boolean sameClass;
+	public static boolean nullIsInstance;
+	public static boolean isInstance;
+	public static boolean isInstance2;
 	
 	private static void strings() {
     	String foo2 = new String(foo);
-    	equal = (foo == foo2); //should be 0
-    	sameIntern = (foo.intern() == foo2.intern()); //should be 1
+    	equal = (foo == foo2);
+    	ass3rt(!equal);
+    	sameIntern = (foo.intern() == foo2.intern());
+    	ass3rt(sameIntern);
 	}
 	
     private static void classes() {
         LinkedList<?> l = new LinkedList<>();
-        sameClass = (l.getClass() == LinkedList.class); //should be 1
+        sameClass = (l.getClass() == LinkedList.class);
+    	ass3rt(sameClass);
+    	nullIsInstance = LinkedList.class.isInstance(null);
+    	ass3rt(!nullIsInstance);
+    	isInstance = LinkedList.class.isInstance(l);
+    	ass3rt(isInstance);
+    	isInstance2 = Number.class.isInstance(Integer.valueOf(1));
+    	ass3rt(isInstance2);
+    	
     }
     
     public static void entryPoint() {
