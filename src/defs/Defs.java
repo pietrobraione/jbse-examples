ddb073b1415d7d358ae602c1ad6865ec7a1062e6
Pietro Braione <pietro.braione@gmail.com>
2019-03-23 21:53:12 +0100

    Small changes

--- src/defs/Defs.java
+++ src/defs/Defs.java
@@ -1,13 +1,13 @@
 package defs;
 
 public class Defs {
-    //Customize them.
+    //Customize them
     public static final String CVC4_PATH         = "/opt/local/bin/cvc4";
     public static final String Z3_PATH           = "/opt/local/bin/z3";
-
-    //Leave them alone.
     public static final String JBSE_HOME         = "/Users/pietro/git/tardis/jbse/";
     public static final String EXAMPLES_HOME     = "/Users/pietro/git/jbse-examples/";
+
+    //Leave them alone
     public static final String JBSE_CLASSPATH    = JBSE_HOME + "build/classes/java/main";
     public static final String JBSE_SOURCEPATH   = JBSE_HOME + "src/main/java/";
     public static final String TARGET_CLASSPATH  = EXAMPLES_HOME + "bin/";
